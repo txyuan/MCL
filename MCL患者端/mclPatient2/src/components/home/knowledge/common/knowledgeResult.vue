a195f628687c733ce38ef925324167eb60913b83
panjunyou <109999@126.com>
2022-02-25 17:53:48 +0800

    优化分享

--- MCL患者端/mclPatient2/src/components/home/knowledge/common/knowledgeResult.vue
+++ MCL患者端/mclPatient2/src/components/home/knowledge/common/knowledgeResult.vue
@@ -113,17 +113,13 @@ export default {
   },
   created() {
     this.getInfoData();
-    // this.onShare()
-    // this.getInfoData();
-    // if(sessionStorage.getItem('share')) {
       this.imgUrl = sessionStorage.getItem('share')
-    // }
     if (this.$route.query.rphone) {
       localStorage.setItem("hphone", this.$route.query.rphone);
     }
   },
   mounted () {
-    this.getWechatParm()
+   
   },
   methods: {
     back() {
@@ -144,7 +140,7 @@ export default {
     wxConfig() {
       let WechatParm = this.WechatParm;
       wx.config({
-        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: WechatParm.AppId, // 必填，公众号的唯一标识
         timestamp: WechatParm.Timestamp, // 必填，生成签名的时间戳
         nonceStr: WechatParm.NonceStr, // 必填，生成签名的随机串
@@ -259,7 +255,6 @@ export default {
       // }
       this.isSmark = true;
       this.showShare = false;
-      console.log(this.imgUrl);
     },
     onShare() {
       let url = "UserInterface/knowledge/UpdateKnowledgeShareCount.ashx";
@@ -282,6 +277,7 @@ export default {
         }
         this.data = res.data;
         this.title = res.data.SubjectName;
+         this.getWechatParm()
         if (res.data.flag == 1) {
           this.good = false;
         } else if (res.data.flag == 2) {
